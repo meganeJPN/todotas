97af90d72cb0057a02c1336ac6ce435932849566
meganeJPN <ooosamuuu1985@gmail.com>
2020-12-21 15:35:19 +0900

    navbarを実装　

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap/dist/js/bootstrap.min
+//= require jquery
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
+// require turbolinks
 //= require_tree .
---
+++ app/javascript/packs/components/header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <nav class="navbar navbar-dark bg-dark">
+      <a href="#" class="navbar-brand">todotas</a>
+      <div class="dropdown">
+        <button
+          type="button"
+          id="dropdownMenuButton"
+          class="btn btn-secondary dropdown-toggle"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          ユーザー名
+        </button>
+        <div
+          class="dropdown-menu"
+          aria-labelledby="dropdownMenuButton"
+          style="z-index:9999"
+        >
+          <a class="dropdown-item" href="#">ユーザ情報</a>
+          <a class="dropdown-item" href="#">ログアウト</a>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
