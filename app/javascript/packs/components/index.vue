196125cda73adfb5f7026bccd8f07e5db5e25502
meganeJPN <ooosamuuu1985@gmail.com>
2020-12-21 21:22:12 +0900

    タスク追加モーダルの作成

--- app/javascript/packs/components/index.vue
+++ app/javascript/packs/components/index.vue
@@ -2,13 +2,6 @@
   <div>
     <!-- 新規作成部分 -->
     <div class="row">
-      <div class="col s10 m11">
-        <input
-          v-model="newTask"
-          class="form-control"
-          placeholder="Add your task!!"
-        />
-      </div>
       <div class="col s2 m1">
         <div
           v-on:click="createTask"
@@ -47,12 +40,27 @@
     <div id="modal1" class="modal">
       <div class="modal-content">
         <h4>Modal Header</h4>
-        <p>A bunch of text</p>
+        <input
+          v-model="newTask"
+          class="form-control"
+          placeholder="Add your task!!"
+        />
+        <input
+          v-model="duration"
+          class="form-control"
+          placeholder="所要時間を入力してください"
+        />
       </div>
       <div class="modal-footer">
         <a href="#!" class="modal-close waves-effect waves-green btn-flat"
           >Agree</a
         >
+        <div
+          v-on:click="createTask"
+          class="btn-floating waves-effect waves-light red"
+        >
+          追加
+        </div>
       </div>
     </div>
 
@@ -86,6 +94,7 @@ export default {
     return {
       tasks: [],
       newTask: '',
+      duration: '',
     };
   },
   mounted: function() {
@@ -110,16 +119,21 @@ export default {
         .classList.toggle('display_none');
     },
     createTask: function() {
-      if (!this.newTask) return;
-      axios.post('/api/tasks', { task: { content: this.newTask } }).then(
-        (resonse) => {
-          this.tasks.unshift(response.data.task);
-          this.newTask = '';
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
+      if (!this.newTask || !this.duration) return;
+      axios
+        .post('/api/tasks', {
+          task: { content: this.newTask, duration: this.duration },
+        })
+        .then(
+          (response) => {
+            this.tasks.unshift(response.data.task);
+            this.newTask = '';
+            this.duration = '';
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
     },
   },
 };
