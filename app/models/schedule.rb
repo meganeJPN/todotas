a90e4496d25c4b5af2c34323fcf042fa9740f1b5
meganeJPN <ooosamuuu1985@gmail.com>
2021-01-14 21:43:40 +0900

    Scheduleモデルにvalidationを追加

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,4 +1,8 @@
 class Schedule < ApplicationRecord
   belongs_to :task
   belongs_to :user
+
+  validates :start_date, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
 end
