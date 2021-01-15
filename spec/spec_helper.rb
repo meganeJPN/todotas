07d8e263d57ba581f5f9f013a0ae2761555294a0
meganeJPN <ooosamuuu1985@gmail.com>
2021-01-15 23:03:46 +0900

    Taskモデルのテストを作成

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -43,7 +43,9 @@ RSpec.configure do |config|
   # inherited by the metadata hash of host groups and examples, rather than
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
-
+  config.before(:all) do
+    FactoryBot.reload
+  end
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
@@ -92,5 +94,6 @@ RSpec.configure do |config|
   # test failures related to randomization by passing the same `--seed` value
   # as the one that triggered the failure.
   Kernel.srand config.seed
+  
 =end
 end
